7fc27b10b1e95c328cbc7e8eb1b854b5d21b0b8d
icemaple <450196683@qq.com>
2021-06-27 08:28:47 +0800

    添加QDebug

--- hcode_demos/main.cpp
+++ hcode_demos/main.cpp
@@ -1,4 +1,5 @@
 #include <QtWidgets>
+#include <QDebug>
 
 int main(int argc, char* argv[])
 {
@@ -8,6 +9,7 @@ int main(int argc, char* argv[])
     QWidget* widget = new QWidget(0,Qt::Dialog | Qt::FramelessWindowHint);  // 无边框样式
     // 设置窗口标题
     widget->setWindowTitle(QObject::tr("我是learnQT from icemaple"));
+    qDebug("widget创建成功");
     // 新建QLabel对象，默认parent参数为0，所以它是个独立窗口
 //    QLabel* label = new QLabel(0,Qt::SplashScreen);  // 欢迎窗口样式
     QLabel* label = new QLabel(0,Qt::SplashScreen | Qt::WindowStaysOnTopHint);  // 欢迎窗口，总是置顶
@@ -17,12 +19,14 @@ int main(int argc, char* argv[])
     label->setText(QObject::tr("我是Label内容：Hello icemaple"));
     // 设置窗口大小
     label->resize(200, 50);
+    qDebug() << QObject::tr("QLabel创建成功");
     // 新建个有parent参数的QLabel，其不再是独立窗口
     QLabel* labelchild = new QLabel(widget);
     // 设置labelchild显示的内容
     labelchild->setText(QObject::tr("我是子QLabel：你好，世界"));
     // 设置labelchild的大小
     labelchild->resize(300, 100);
+    qDebug("labelchid.x=%d", labelchild->geometry().x());
     // show
     label->show();
     widget->show();
