a633605418c2ec6599e2bf0bc3d15416120edce5
icemaple <450196683@qq.com>
2021-06-27 09:12:05 +0800

    add signal and solt function, and connect them via manual

--- qt_demos/mainwindow.cpp
+++ qt_demos/mainwindow.cpp
@@ -1,16 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDialog>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->showChildButton, &QPushButton::clicked, this, &MainWindow::showChildDialog);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::showChildDialog()
+{
+    QDialog *dialog = new QDialog(this);
+    dialog->show();
 }
 
 
